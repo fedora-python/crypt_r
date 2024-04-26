04d2896986ef9391b65e05e35b4e6a0e8f0c4b1f
Tomáš Hrnčiar <thrnciar@redhat.com>
2024-04-26 12:02:24 +0200

    Inline argument clinic code and remove private Python API
    Co-authored-by: Petr Viktorin <encukou@gmail.com>
    Co-authored-by: Karolina Surma <ksurma@redhat.com>

--- Modules/_cryptmodule.c
+++ Modules/_cryptmodule.c
@@ -1,4 +1,6 @@
 /* cryptmodule.c - by Steve Majewski
+ *
+ * Taken from Python 3.12 with removed argument clinic code.
  */
 
 #include "Python.h"
@@ -10,33 +12,67 @@
 
 /* Module crypt */
 
-/*[clinic input]
-module crypt
-[clinic start generated code]*/
-/*[clinic end generated code: output=da39a3ee5e6b4b0d input=c6252cf4f2f2ae81]*/
-
-#include "clinic/_cryptmodule.c.h"
-
-/*[clinic input]
-crypt.crypt
-
-    word: str
-    salt: str
-    /
-
-Hash a *word* with the given *salt* and return the hashed password.
-
-*word* will usually be a user's password.  *salt* (either a random 2 or 16
-character string, possibly prefixed with $digit$ to indicate the method)
-will be used to perturb the encryption algorithm and produce distinct
-results for a given *word*.
-
-[clinic start generated code]*/
+PyDoc_STRVAR(crypt_crypt__doc__,
+"crypt($module, word, salt, /)\n"
+"--\n"
+"\n"
+"Hash a *word* with the given *salt* and return the hashed password.\n"
+"\n"
+"*word* will usually be a user\'s password.  *salt* (either a random 2 or 16\n"
+"character string, possibly prefixed with $digit$ to indicate the method)\n"
+"will be used to perturb the encryption algorithm and produce distinct\n"
+"results for a given *word*.");
 
 static PyObject *
-crypt_crypt_impl(PyObject *module, const char *word, const char *salt)
-/*[clinic end generated code: output=0512284a03d2803c input=0e8edec9c364352b]*/
+crypt_crypt(PyObject *module, PyObject *const *args, Py_ssize_t nargs)
 {
+    if (nargs != 2) {
+        PyErr_Format(PyExc_TypeError,"crypt expected 2 arguments, got %zd", nargs);
+        return NULL;
+    }
+    const char *word;
+    const char *salt;
+    Py_ssize_t word_length;
+    Py_ssize_t salt_length;
+
+    if (!PyUnicode_Check(args[0])) {
+        PyErr_Format(
+            PyExc_TypeError, "crypt argument 1 (word) must be string, not %s",
+            Py_TYPE(args[0])->tp_name
+        );
+        return NULL;
+    }
+    word = PyUnicode_AsUTF8AndSize(args[0], &word_length);
+    if (word == NULL) {
+        return NULL;
+    }
+    if (strlen(word) != (size_t)word_length) {
+        PyErr_SetString(
+            PyExc_ValueError,
+            "crypt argument 1 (word) contains embedded null character"
+        );
+        return NULL;
+    }
+
+    if (!PyUnicode_Check(args[1])) {
+        PyErr_Format(
+            PyExc_TypeError, "crypt argument 2 (salt) must be string, not %s",
+            Py_TYPE(args[1])->tp_name
+        );
+        return NULL;
+    }
+    salt = PyUnicode_AsUTF8AndSize(args[1], &salt_length);
+    if (salt == NULL) {
+        return NULL;
+    }
+    if (strlen(salt) != (size_t)salt_length) {
+        PyErr_SetString(
+            PyExc_ValueError,
+            "crypt argument 2 (salt) contains embedded null character"
+        );
+        return NULL;
+    }
+
     char *crypt_result;
 #ifdef HAVE_CRYPT_R
     struct crypt_data data;
@@ -48,12 +84,12 @@ crypt_crypt_impl(PyObject *module, const char *word, const char *salt)
     if (crypt_result == NULL) {
         return PyErr_SetFromErrno(PyExc_OSError);
     }
-    return Py_BuildValue("s", crypt_result);
+    return PyUnicode_FromString(crypt_result);
 }
 
 
 static PyMethodDef crypt_methods[] = {
-    CRYPT_CRYPT_METHODDEF
+    {"crypt", crypt_crypt, METH_FASTCALL, crypt_crypt__doc__},
     {NULL,              NULL}           /* sentinel */
 };
 
